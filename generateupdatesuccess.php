937adb29ba10000e7b8b16debd20bc1693b723b3
Harsh Satish Singh <56669968+HarshSingh74@users.noreply.github.com>
2020-11-07 01:23:11 +0530

    Add files via upload

---
+++ generateupdatesuccess.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <style>
+          .bg-image{
+    /* The image used */
+    background-image:url("images/billsuccess.JPG");
+
+  }
+    </style>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<link rel="stylesheet" href="css/invalidstyle1.css">
+</head>
+<body>
+<div class="bg-image"></div>
+<div class="bg-text">
+<h2>Bill Format Updated Successfully!</h2>
+<a href="layout backup/admindashboard.php" class="try">BACK TO DASHBOARD</a>
+</div>
+</body>
+</html>
