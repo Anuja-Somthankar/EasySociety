fb4cf88f751e454673cedc65474988ddcbed55d2
Harsh Satish Singh <56669968+HarshSingh74@users.noreply.github.com>
2020-11-07 01:19:32 +0530

    Add files via upload

---
+++ addmemberssuccess.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <style>
+          .bg-image{
+    /* The image used */
+    background-image:url("images/addsuccess.JPG");
+
+  }
+    </style>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<link rel="stylesheet" href="css/invalidstyle1.css">
+</head>
+<body>
+<div class="bg-image"></div>
+<div class="bg-text">
+<h2>A new Member Entry Generated Successfully.</h2>
+<a href="layout backup/add_members.html" class="try">BACK TO DASHBOARD</a>
+</div>
+</body>
+</html>
---
+++ addvisitorsuccess.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <style>
+          .bg-image{
+    /* The image used */
+    background-image:url("images/visitorsuccess.JPG");
+
+  }
+    </style>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<link rel="stylesheet" href="css/invalidstyle1.css">
+</head>
+<body>
+<div class="bg-image"></div>
+<div class="bg-text">
+<h2>Visitor Record Entered Successfully.</h2>
+<a href="layout backup/add_visitors.html" class="try">BACK</a>
+</div>
+</body>
+</html>
